896d8d8f008bef3905662cf945bd1d41e88403ec
anuragdevon <msm19b021@iiitdm.ac.in>
2023-03-03 03:15:00 +0530

    create basic model setup

--- passwords/models.py
+++ passwords/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Password(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    website_name = models.CharField(max_length=200)
+    username = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.website_name
+
