fbaf2dfea56e5f3d4473f5e05bf5fba042cf6b8d
anuragdevon <msm19b021@iiitdm.ac.in>
2023-03-03 20:05:38 +0530

    add new user models

--- passwords/models.py
+++ passwords/models.py
@@ -1,12 +1,13 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    pass
 
 class Password(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    website_name = models.CharField(max_length=200)
-    username = models.CharField(max_length=200)
-    password = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.website_name
-
+    website = models.CharField(max_length=255)
+    username = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
