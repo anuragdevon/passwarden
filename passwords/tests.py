21ea11cf05beb5f28bd5db2b10ceaaa9a0c991f0
anuragdevon <msm19b021@iiitdm.ac.in>
2023-03-09 19:58:14 +0530

    add setup tests for views

--- passwords/tests.py
+++ passwords/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from rest_framework.test import APITestCase
+from .models import Password
+from .serializers import PasswordSerializer
 
-# Create your tests here.
+User = get_user_model()
+
+class PasswordTests(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+        self.client.login(username='testuser', password='testpass')
+        self.password = Password.objects.create(user=self.user, website='example.com', username='testuser', password='password')
+
